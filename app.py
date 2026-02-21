9a262e7906f3263f755a9936b4f10b124757b83a
sadhanap2006 <sadhanap2006@gmail.com>
2026-02-21 23:11:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import easyocr
+import numpy as np
+from PIL import Image
+import re
+import pyttsx3
+
+st.title("AI Smart Product Scanner")
+
+reader = easyocr.Reader(['en'])
+
+uploaded_file = st.file_uploader("Upload Product Image", type=["jpg", "png", "jpeg"])
+
+def extract_info(text):
+    expiry = re.findall(r'\b\d{2}/\d{2}/\d{4}\b', text)
+    mrp = re.findall(r'MRP\s?[:\-]?\s?\d+', text, re.IGNORECASE)
+
+    allergens = []
+    allergy_keywords = ["peanut", "milk", "soy", "gluten", "nuts"]
+    for word in allergy_keywords:
+        if word in text.lower():
+            allergens.append(word)
+
+    return expiry, mrp, allergens
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    img_array = np.array(image)
+
+    result = reader.readtext(img_array, detail=0)
+    full_text = " ".join(result)
+
+    expiry, mrp, allergens = extract_info(full_text)
+
+    st.subheader("Detected Information")
+
+    st.write("Expiry Date:", expiry)
+    st.write("MRP:", mrp)
+    st.write("Allergens:", allergens)
+
+    speech_text = f"Expiry {expiry}. MRP {mrp}. Allergens {allergens}"
+    speak(speech_text)
